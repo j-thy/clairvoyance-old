09cff79f68ba21428e5e7adcded468b60e266308
Jonathan Ting <jonathan.ting07@gmail.com>
2023-02-18 21:57:46 -0500

    Changed timezone

--- django_inertia_svelte/settings.py
+++ django_inertia_svelte/settings.py
@@ -102,8 +102,8 @@ class Base(Configuration):
 
     # Internationalization
     # https://docs.djangoproject.com/en/4.1/topics/i18n/
-    LANGUAGE_CODE = 'it-IT'
-    TIME_ZONE = 'Europe/Rome'
+    LANGUAGE_CODE = 'en-us'
+    TIME_ZONE = 'UTC'
     USE_I18N = True
     USE_TZ = True
 
