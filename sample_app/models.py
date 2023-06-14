b111c94b51884a9e826cb55f7558600e3e944360
jjting <jting@vols.utk.edu>
2023-06-13 21:17:39 -0400

    Initial models

--- sample_app/models.py
+++ sample_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Banner(models.Model):
+    jp_name = models.CharField(max_length=200, blank=True, default='')
+    en_name = models.CharField(max_length=200, blank=True, default='')
+    jp_wiki_link = models.CharField(max_length=200, blank=True, default='')
+    en_wiki_link = models.CharField(max_length=200, blank=True, default='')
+    jp_start_date = models.DateField(blank=True, null=True)
+    en_start_date = models.DateField(blank=True, null=True)
+    jp_end_date = models.DateField(blank=True, null=True)
+    en_end_date = models.DateField(blank=True, null=True)
+    jp_banner_id = models.CharField(max_length=200, blank=True, default='')
+    en_banner_id = models.CharField(max_length=200, blank=True, default='')
+
+    def __str__(self):
+        return self.en_name if not '' else self.jp_name
+        
+
+class Servant(models.Model):
+    servant_id = models.IntegerField(primary_key=True)
+    status = models.CharField(max_length=200, blank=True, default='')
+    name = models.CharField(max_length=200, blank=True, default='')
+    rateups = models.ManyToManyField(Banner)
+    pass
+
+    def __str__(self):
+        return self.name
