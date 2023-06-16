9bf648074db494127660e11708a5361fd351c4af
jjting <jting@vols.utk.edu>
2023-06-15 22:12:26 -0400

    Add import for Servants

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
 from .models import Banner, Servant
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
+from import_export.widgets import ManyToManyWidget
+
+class ServantResource(resources.ModelResource):
+    rateups = fields.Field(
+        column_name='rateups',
+        attribute='rateups',
+        # Separator for JSON list
+        widget=ManyToManyWidget(Banner, field='banner_id')
+    )
+    class Meta:
+        model = Servant
+        skip_unchanged = True
+        report_skipped = False
+        import_id_fields = ('servant_id',)
+
+class ServantAdmin(ImportExportModelAdmin):
+    resource_classes = [ServantResource]
+    
 
 admin.site.register(Banner)
-admin.site.register(Servant)
+admin.site.register(Servant, ServantAdmin)
