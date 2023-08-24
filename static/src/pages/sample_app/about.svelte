bd97402b17c298e6460977d3b1b2f05d4a85f2a9
Akuplace <103694909+Akuplace@users.noreply.github.com>
2023-08-24 16:44:26 +0100

    Akudev (#1)
    Added tailwindcss

--- static/src/pages/sample_app/about.svelte
+++ static/src/pages/sample_app/about.svelte
@@ -7,6 +7,6 @@
 
 <ul>
 {#each $page.props.events as event}
-    <li><a href="/about/{event}">{event}</a></li>
+    <li class="underline"><a href="/about/{event}">{event}</a></li>
 {/each}
 </ul>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./static/src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
